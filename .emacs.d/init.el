8347059a692d66c14a5b7f1b332477b1648c84be
yuuki <yukifrog@users.noreply.github.com>
2020-10-24 22:44:04 +0900

    setting emacs skk

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,7 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (straight-use-package 'flymake-ruby)
+;flymake
 (straight-use-package 'robe)
 ;; M-x inf-ruby
 ;; M-x robe-start
@@ -30,10 +31,14 @@
   '(push 'company-robe company-backends))
 
 (straight-use-package 'helm)
+;(straight-use-package 'consel)
+(straight-use-package 'ivy)
+(straight-use-package 'swiper)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
+(global-set-key (kbd "C-s") 'swiper)
 
 (straight-use-package 'ruby-block)
 (straight-use-package 'ruby-electric)
@@ -50,6 +55,41 @@
 ;;dump-jump
 ;;flyspell
 ;;rinari
+;tag?
 ;;migemo
+;;magit
+(straight-use-package 'forge)
+(straight-use-package 'magit)
+;;yasnippet
+;;git-company
+;;neotree
+
 
 (straight-use-package 'yaml-mode)
+
+
+;show-paren-mode
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+
+      
+(tool-bar-mode -1) 
+
+;https://emacs-jp.github.io/packages/anzu
+(straight-use-package 'anzu) 
+(global-anzu-mode t)
+(setq anzu-search-threshold 1000)
+;; migemoを利用している場合
+;(setq anzu-use-migemo t)
+
+
+;skk
+;https://ddskk.readthedocs.io/ja/latest/
+(straight-use-package 'ddskk)
+(global-set-key (kbd "C-x C-j") 'skk-mode)
+;(global-set-key (kbd "<zenkaku-hankaku>") 'skk-mode)
+(global-set-key (kbd "<zenkaku-hankaku>") 'skk-auto-fill-mode)
+(setq skk-show-mode-show t)
+(setq skk-show-mode-style 'tooltip)
+;C-h v 変数の中身を確認
+;backspase C-h
