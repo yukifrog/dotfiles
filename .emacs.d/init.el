2fd4c9f2753d814ab7ac8a0a70672fe3c01092a8
yuuki <yukifrog@users.noreply.github.com>
2020-10-26 17:01:23 +0900

    whitespace

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,9 +4,9 @@
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -30,6 +30,17 @@
 
 ;;whitespace-mode
 ;;https://qiita.com/itiut@github/items/4d74da2412a29ef59c3a
+(require 'whitespace)
+;; 保存前に自動でクリーンアップ
+(setq whitespace-action '(auto-cleanup))
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+(add-hook 'markdown-mode-hook
+	  '(lambda ()
+	     (set (make-local-variable 'whitespace-action) nil)))
+;(global-whitespace-mode 1)
+
+
 
 
 
@@ -39,13 +50,13 @@
 ;;rubocop flycheck lint
 ;;gem install rubocop ruby-lint pry pry-dock reek
 (add-hook 'ruby-mode-hook
-          '(lambda ()
-             (setq flycheck-checker 'ruby-rubocop)
-             (flycheck-mode 1)))
+	  '(lambda ()
+	     (setq flycheck-checker 'ruby-rubocop)
+	     (flycheck-mode 1)))
 ;; 自動起動
 (setq flycheck-check-syntax-automatically
   '(save idle-change mode-enabled))
-    
+
 ;; コード変更後、3秒後にチェックする
 (setq flycheck-idle-change-delay 3)
 ;;(setq flycheck-check-syntax-automatically '(idle-change mode-enabled new-line save))
@@ -63,11 +74,26 @@
 
 
 
+
 ;;hl-line-mode
 ;;https://www.emacswiki.org/emacs/HighlightCurrentLine
-(global-hl-line-mode t)
+;;(global-hl-line-mode -1)
 
+;;https://github.com/mahito1594/dotemacs
+(straight-use-package 'beacon)
+(setq beacon-blink-duration 1)
+(setq beacon-color "white")
+(setq beacon-dont-blink-commands nil)
+(add-hook 'after-init-hook 'beacon-mode)
 
+;;https://github.com/k-talo/volatile-highlights.el
+(straight-use-package 'volatile-highlights)
+(volatile-highlights-mode t)
+
+;;https://github.com/DarthFennec/highlight-indent-guides
+(straight-use-package 'highlight-indent-guides)
+(add-hook 'emacs-lisp-mode-hook 'highlight-indent-guides-mode)
+(add-hook 'ruby-mode-hook 'highlight-indent-guides-mode)
 
 
 (straight-use-package 'robe)
@@ -89,6 +115,18 @@
 (straight-use-package 'helm)
 ;;helm helm-config?
 ;(straight-use-package 'consel)
+;;ivy-rich
+;; https://qiita.com/Ladicle/items/feb5f9dce9adf89652cf
+
+
+;; ace-window
+;; https://github.com/abo-abo/ace-window
+
+
+;;lsp-mode?
+;;https://github.com/emacs-lsp/lsp-mode
+
+
 (straight-use-package 'ivy)
 (straight-use-package 'swiper)
 (global-set-key (kbd "M-x") #'helm-M-x)
@@ -104,7 +142,10 @@
 (global-set-key (kbd "C-s") 'swiper)
 
 (straight-use-package 'ruby-block)
+(require 'ruby-block) ;; <- なぜか必要
+(ruby-block-mode t)
 (straight-use-package 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 (straight-use-package 'pry)
 (straight-use-package 'inf-ruby)
 
@@ -114,9 +155,15 @@
 (nyan-mode t)
 
 ;;rbenv
-;;ruby end
+;;ruby end <- ruby-electricとかぶってる
 ;;projectile?
-;;smart-newline.el
+
+
+;;smart-newline.el https://github.com/ainame/smart-newline.el
+(straight-use-package 'smart-newline)
+(add-hook 'ruby-mode-hook
+	  (lambda ()
+	    (smart-newline-mode 1)))
 ;;dump-jump
 ;;flyspell
 ;;rinari
@@ -127,13 +174,26 @@
 (straight-use-package 'magit)
 
 (straight-use-package 'yasnippet)
-(yas-global-mode t) 
+(yas-global-mode t)
 (straight-use-package 'yasnippet-snippets)
 
 
 ;;git-company
-;;neotree
 
+;;neotree
+;;https://github.com/jaypei/emacs-neotree
+(straight-use-package 'neotree)
+
+;;imenu
+(straight-use-package 'imenu-list)
+
+
+;;minimap
+
+
+;;hide-mode-line
+;;amex semex
+;;hydra https://github.com/abo-abo/hydra
 
 (straight-use-package 'yaml-mode)
 
@@ -142,11 +202,13 @@
 (setq show-paren-delay 0)
 (show-paren-mode 1)
 
-      
-(tool-bar-mode -1) 
+;括弧の自動挿入
+(electric-pair-mode 1)
+
+(tool-bar-mode -1)
 
 ;https://emacs-jp.github.io/packages/anzu
-(straight-use-package 'anzu) 
+(straight-use-package 'anzu)
 (straight-use-package 'migemo)
 
 (global-anzu-mode t)
@@ -174,3 +236,10 @@
 ;; (which-key-setup-side-window-right)     ;右端
 ;; (which-key-setup-side-window-right-bottom) ;両方使う
 (which-key-mode 1)
+
+(straight-use-package 'rainbow-delimiters)
+;(rainbow-delimiters-mode-enable)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode-enable)
+
+(provide 'init)
+;;; init.el ends here
